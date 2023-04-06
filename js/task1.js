2d82f928de06a9b66295609f4d1e612e19f1c175
Valentin Kim <valentink@yandex.ru>
2023-04-06 18:01:00 +0600

    в задании 1 реализована стилизации уведомления (выпадание)

--- js/task1.js
+++ js/task1.js
@@ -1,19 +1,20 @@
-$("#add-item-btn").click(function(){
-    const div = document.createElement('div');
-    $('#container').append(div);
-    $(div).append('If you click on the "Close" button, I will disappear');
-    
-    div.className = 'class'
-    const input = document.createElement('input');
-    $('#container').append(input);
-    input.id = 'delete';
-    input.type = 'button';
-    input.value = 'Закрыть';
-    $(div).hide(5000);
-    
-    $("#delete").click(function(){
-    $('.class').remove();
-    $("#delete").remove();
+$(document).ready(function(){
+    $("#add-item-btn").click(function(){
+        const input = document.createElement('input');
+        $('#container').append(input);
+        input.id = 'delete';
+        input.type = 'button';
+        input.value = 'Закрыть';
+        
+        const div = document.createElement('div');
+        $('#container').append(div);
+        div.className = 'class'
+        $(div).append('If you click on the "Close" button, I will disappear').slideDown("slow").slideUp(5000);
+        
+        $("#delete").fadeOut(5000).click(function(){
+        $('.class').remove();
+        $("#delete").remove();
+        });
     });
 });
 
