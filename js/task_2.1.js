0908c6bb751e068d91a7ada01d47efbf32db7d52
Valentin Kim <valentink@yandex.ru>
2023-04-07 17:38:38 +0600

    Задание 2: корректировки в разметках

--- js/task_2.1.js
+++ js/task_2.1.js
@@ -1,9 +1,10 @@
 let urlParams = new URLSearchParams(window.location.search);
 let param = urlParams.get('id');
-const image = document.querySelector('img')
+const image = document.createElement('img')
 const h1 = document.createElement('h1');    
 const h2 = document.createElement('h2');    
 const ul = document.createElement('ul');
+const ul2 = document.createElement('ul');
 ul.className = 'description'
 
 $.ajax({
@@ -21,34 +22,36 @@ $.ajax({
 
 
 function addInfo(character) {
-    $(image).before(h1)
-    $(h1).append(`Character: ${character.name}`)
+    $('.container').append(image)
     image.src=character.image
-    h2.innerHTML = 'Episodes:'
-    $('#episode').before(h2);
+    $(image).before(h1);
+    $(h1).append(`Character: ${character.name}`);
+    $('.container').append(ul)
     
-    $(ul).append(`<li><b>Name:</b> ${character.name}</li>`);
-    $(ul).append(`<li><b>Created:</b> ${character.created}</li>`);
-    $(ul).append(`<li><b>Gender:</b> ${character.gender}</li>`);
-    $(ul).append(`<li><b>Id:</b> ${character.id}</li>`);
-    $(ul).append(`<li><b>Location name:</b> ${character.location.name}</li>`);
-    $(ul).append(`<li><b>Location url:</b> ${character.location.url}</li>`);
-    $(ul).append(`<li><b>Origin:</b> ${character.origin}</li>`);
-    $(ul).append(`<li><b>Species:</b> ${character.species}</li>`);
-    $(ul).append(`<li><b>Status:</b> ${character.status}</li>`);
-    $(ul).append(`<li><b>Type:</b> ${character.type}</li>`);
-    $(ul).append(`<li><b>Url:</b> ${character.url}</li>`);
-
-    $('h2').before(ul);
+    $('.description').append(`<li><b>Name:</b> ${character.name}</li>`);
+    $('.description').append(`<li><b>Created:</b> ${character.created}</li>`);
+    $('.description').append(`<li><b>Gender:</b> ${character.gender}</li>`);
+    $('.description').append(`<li><b>Id:</b> ${character.id}</li>`);
+    $('.description').append(`<li><b>Location name:</b> ${character.location.name}</li>`);
+    $('.description').append(`<li><b>Location url:</b> ${character.location.url}</li>`);
+    $('.description').append(`<li><b>Origin:</b> ${character.origin}</li>`);
+    $('.description').append(`<li><b>Species:</b> ${character.species}</li>`);
+    $('.description').append(`<li><b>Status:</b> ${character.status}</li>`);
+    $('.description').append(`<li><b>Type:</b> ${character.type}</li>`);
+    $('.description').append(`<li><b>Url:</b> ${character.url}</li>`);
+    
     
     updateEpisodes(character.episode)
 };
 
 
 const updateEpisodes = (episodes) => {
+    $('.container').append(ul2)
+    h2.innerHTML = 'Episodes:'
+    $(ul2).before(h2);
     for (let episode of episodes) {
         let episode_str = `<li>${episode}`;
-        $('#episode').append(episode_str)
+        $(ul2).append(episode_str)
         }
     }
 
