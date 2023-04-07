4b2712c6a8ca539d64c96bad3f3a55143459f78f
Valentin Kim <valentink@yandex.ru>
2023-04-07 16:47:45 +0600

    Задание 2: реализован выввод всех полей персонажа

--- js/task_2.1.js
+++ js/task_2.1.js
@@ -1,9 +1,10 @@
 let urlParams = new URLSearchParams(window.location.search);
 let param = urlParams.get('id');
-
-
 const image = document.querySelector('img')
-
+const h1 = document.createElement('h1');    
+const h2 = document.createElement('h2');    
+const ul = document.createElement('ul');
+ul.className = 'description'
 
 $.ajax({
     url: `https://rickandmortyapi.com/api/character/${param}`,
@@ -20,14 +21,26 @@ $.ajax({
 
 
 function addInfo(character) {
-    $('h1').append(character.name)
-    const h2 = document.createElement('h2');    
+    $(image).before(h1)
+    $(h1).append(`Character: ${character.name}`)
+    image.src=character.image
     h2.innerHTML = 'Episodes:'
     $('#episode').before(h2);
     
-    image.src=character.image
-    console.log(character.name)
+    $(ul).append(`<li><b>Name:</b> ${character.name}</li>`);
+    $(ul).append(`<li><b>Created:</b> ${character.created}</li>`);
+    $(ul).append(`<li><b>Gender:</b> ${character.gender}</li>`);
+    $(ul).append(`<li><b>Id:</b> ${character.id}</li>`);
+    $(ul).append(`<li><b>Location name:</b> ${character.location.name}</li>`);
+    $(ul).append(`<li><b>Location url:</b> ${character.location.url}</li>`);
+    $(ul).append(`<li><b>Origin:</b> ${character.origin}</li>`);
+    $(ul).append(`<li><b>Species:</b> ${character.species}</li>`);
+    $(ul).append(`<li><b>Status:</b> ${character.status}</li>`);
+    $(ul).append(`<li><b>Type:</b> ${character.type}</li>`);
+    $(ul).append(`<li><b>Url:</b> ${character.url}</li>`);
 
+    $('h2').before(ul);
+    
     updateEpisodes(character.episode)
 };
 
